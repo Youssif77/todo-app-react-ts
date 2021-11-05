b9206947ca8e0c1641ed9deb6ccc06a9f1fd79ec
youssif hany <youssifhany75@gmail.com>
2021-11-05 22:27:18 +0200

    add add-todo-func

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,24 @@
+import { useRef } from "react";
+
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (e: React.FormEvent) => {
+    e.preventDefault();
+    const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) return;
+
+    props.onAddTodo(enteredText);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="text">Todo text</label>
+      <input ref={todoTextInputRef} id="text" type="text" />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
